d20048b3a060dfb6869e8657134b2611e0d4d478
Erfan Besharat <erbesharat@gmail.com>
2019-02-25 18:17:24 +0330

    (main): Add ability to join rooms

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const sql = require("mssql");
-const uuid = require("uuid");
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const sql = require('mssql');
+const uuid = require('uuid');
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/static/pages/index.html");
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/static/pages/index.html');
 });
 
-io.on("connection", function (socket) {
+const boilMSSQL = query => [
+  query.replace(/%db/g, '[WellinnoApiDBTestWebsocket]'),
+];
 
+function handleWebsocket(socket) {
   //
   // ─── SOCKET EVENTS ──────────────────────────
   //
-
 
   // ********** Request Model **********
   //  {
@@ -21,22 +23,40 @@ io.on("connection", function (socket) {
   //    "recipient_id": int,
   //  }
   // ***********************************
-  socket.on("check", async function (data) {
-    // TODO: Connect database and search for user
-    // TODO: Connect database and search for recipient
-    roomID = uuid.v4();
-  });
-
-  socket.on("chat message", async function (msg) {
-    try {
-      const result = await sql.query`select * from users`
-      io.emit("chat response", result)
-    } catch (err) {
-      console.error("[websocket-server] Error (#16): ", err)
+  socket.on('request', async function(data) {
+    const result = await sql.query(
+      boilMSSQL(
+        `SELECT * FROM %db.[dbo].[rooms] WHERE user_id = ${
+          data.user_id
+        } AND recipient_id = ${data.recipient_id};`,
+      ),
+    );
+    if (result.rowsAffected < 1) {
+      const roomID = uuid.v4();
+      const insertResult = await sql.query(
+        boilMSSQL(
+          `INSERT INTO %db.[dbo].[rooms] (title, user_id, recipient_id)
+          VALUES ('${roomID}', ${data.user_id}, ${data.recipient_id})
+          `,
+        ),
+      );
+      socket.join(roomID);
+      socket.emit('request response', 'joined to ' + roomID);
+    } else {
+      socket.join(result.recordset[0].title);
+      socket.emit('request response', 'joined to ' + result.recordset[0].title);
     }
   });
-});
+}
 
-http.listen(3000, function () {
-  console.log("listening on *:3000");
-});
+async function startServer() {
+  await sql.connect(
+    'mssql://Erfan:34uxwp7Mco7@185.211.57.185/WellinnoApiDBTestWebsocket',
+  );
+  io.on('connection', handleWebsocket);
+  http.listen(4000, function() {
+    console.log('listening on *:4000');
+  });
+}
+
+startServer();
---
+++ src/main_test.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+const io = require('socket.io-client');
+
+test('It should test check listener', async t => {
+  const request = {
+    user_id: 1,
+    recipient_id: 1,
+  };
+
+  const socket = io.connect('http://localhost:4000');
+  socket.emit('request', request);
+  await new Promise((resolve, reject) => {
+    socket.on('request response', function(msg) {
+      console.log(msg);
+      resolve();
+    });
+  });
+
+  t.pass();
+});
