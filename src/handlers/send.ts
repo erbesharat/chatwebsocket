a91958ed9b56e69ed292797c6271150a156ff2bc
Erfan Besharat <erbesharat@gmail.com>
2019-03-16 15:14:50 +0330

    Add debugging logs

--- src/handlers/send.ts
+++ src/handlers/send.ts
@@ -7,8 +7,10 @@ export default (socket: Socket) => async (data: Message) => {
   const result = await sql.query(
     boilMSSQL(`SELECT * FROM %db.[rooms] WHERE title = '${data.room_title}';`),
   );
+  console.log('\n(SEND MESSAGE) Room Object: \n' + result);
 
   if (result.rowsAffected[0] === 0) {
+    console.log('\n(SEND MESSAGE) Room does not exist');
     socket.emit(
       'logs response',
       `[error]: room ${data.room_title} doesn't exist`,
@@ -19,7 +21,11 @@ export default (socket: Socket) => async (data: Message) => {
   console.log('\n\nResult:\n', result);
 
   const { id, user_id, recipient_id } = result.recordset[0];
-  if (user_id !== data.user_id || recipient_id !== data.user_id) {
+  if (user_id != data.user_id || recipient_id != data.user_id) {
+    console.log('\n(SEND MESSAGE) Something is wrong with ids!\n');
+    console.log(
+      '\n(SEND MESSAGE) IDs: !\n' + id + ',' + user_id + ',' + recipient_id,
+    );
     socket.emit(
       'logs response',
       `[error]: user doesn't belong to room ${data.room_title}`,
@@ -28,6 +34,7 @@ export default (socket: Socket) => async (data: Message) => {
   }
 
   try {
+    console.log('\n(SEND MESSAGE) Goest to create the message in DB!\n');
     await sql.query(
       boilMSSQL(
         `INSERT INTO %db.[chat_messages] (author_id, room_id, text, created_at)
