2ac916519fc4c2c50ecee63810c288ff3045fb60
Erfan Besharat <erbesharat@gmail.com>
2019-05-01 18:58:11 +0430

    Add logs

--- src/handlers/disconnect.ts
+++ src/handlers/disconnect.ts
@@ -4,6 +4,14 @@ import { boilMSSQL } from '../utils/mssql';
 import moment from 'moment-jalaali';
 
 export default (socket: Socket, perm) => async data => {
+  console.log(
+    '\n\n===Disconnect Query===\n\n',
+    boilMSSQL(
+      `UPDATE %db.[Users] SET IsOnline = 0, lastSeen = '${moment().format(
+        'jYYYY/jM/jD HH:mm',
+      )}' WHERE id = ${perm.user_id};`,
+    ),
+  );
   const result = await sql.query(
     boilMSSQL(
       `UPDATE %db.[Users] SET IsOnline = 0, lastSeen = '${moment().format(
